8c7e3a7c66f56c8d76af18c1a120e9dca0f9cf91
Bruna do Nascimento da Silva <bruna-nascimento_@hotmail.com>
2021-09-14 19:58:33 -0300

    finalizado html e css do desafio calcular media

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var notaPrimeiroBimestre = 9
+var notaSegundoBimestre = 10
+var notaTerceroBimestre = 7 
+var notaQuartoBimestre = 6
+
+var notaFinal = ( notaPrimeiroBimestre + notaSegundoBimestre + notaTerceroBimestre + notaQuartoBimestre ) / 4 
+
+var notaFixada = notaFinal.toFixed(1)
+
+
